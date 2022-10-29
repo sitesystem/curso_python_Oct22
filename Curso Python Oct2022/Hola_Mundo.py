688b18297ed2fabe46c9f6aeff2fcd324a41fc6d
sitesystem <l_aloaxa05@hotmail.com>
2022-10-29 10:44:52 -0500

    1ra subida de archivo

---
+++ Curso Python Oct2022/Hola_Mundo.py
@@ -0,0 +1,19 @@
+from distutils.sysconfig import get_config_h_filename
+
+
+hgfhfgh
+get_config_h_filename
+ghfghgh
+fghfghfgh
+fghfghfghfgh
+fghfghfghfgh
+ghfghfghfghfgh
+
+ghfghfghfghfgh
+
+dfdffdfdfdf
+
+
+oiioio
+
+
